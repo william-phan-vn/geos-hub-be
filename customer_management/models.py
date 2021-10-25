247faa5871ae5f55b6a947044b3434db1f43182e
William Phan <hoangviet191@gmail.com>
2021-10-26 00:17:46 +0700

    Update models

---
+++ customer_management/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from service_management.models import ServicePrice
+
+
+class Customer(models.Model):
+    email = models.EmailField(blank=False)
+    password = models.CharField(max_length=100)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+
+class PaidCustomer(models.Model):
+    description = models.CharField(max_length=100)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    service = models.ForeignKey(ServicePrice, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.description
